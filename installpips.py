eeb3a6eac9b567344fe12cf2f02b71c9d19a7487
Tempfyyyy <136855015+Tempfyyyy@users.noreply.github.com>
2023-06-28 13:55:46 +0200

    installpips.py

--- installpips.py
+++ installpips.py
@@ -1,15 +1,12 @@
-import subprocess
+import os
 
-def install_packages():
-    packages = ['ctypes', 'requests', 'colorama']
+required_packages = [
+    "requests",
+    "colorama"
+]
 
-    for package in packages:
-        try:
-            subprocess.check_call(['pip', 'install', package])
-            print(f"Successfully installed {package}")
-        except subprocess.CalledProcessError as e:
-            print(f"Error installing {package}: {e}")
-        except Exception as e:
-            print(f"Unexpected error occurred while installing {package}: {e}")
+def install_package(package_name):
+    os.system(f"pip install {package_name}")
 
-install_packages()
+for package in required_packages:
+    install_package(package)
